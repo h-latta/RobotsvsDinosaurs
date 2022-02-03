cbbdb28622065287d20abd5b0fd7ab42534eb0e8
h-latta <h.latta@yahoo.com>
2022-02-03 13:48:04 -0600

    Added Welcome Message and Battle Roster

--- battlefield.py
+++ battlefield.py
@@ -1,3 +1,4 @@
+from os import name
 from fleet import Fleet
 from herd import Herd
 
@@ -7,15 +8,19 @@ herd = Herd()
 class Battlefield:
     def __init__(self):
         self.game = self.run_game()
+        self.name = name
 
-    def run_game(self):
-        pass
-        
     def display_welcome(self):
-        pass
+        print('ROBOTS VS DINOSAURS!')
+        print("Welcome to the arena. Let's get going!")
+        self.name = input("For starters, what's your name? ")
+        print(f'Okay {self.name}, get ready to battle!')
     
     def battle(self):
-        pass
+        fleet.create_fleet()
+        herd.create_herd()
+        print(f'For the robots, we have {fleet.army}.')
+        print(f'For the dinosaurs, we have {herd.group}.')
 
     def dino_turn(self, dinosaur):
         pass
@@ -31,3 +36,10 @@ class Battlefield:
 
     def display_winners(self):
         pass
+
+    def run_game(self):
+        Battlefield.display_welcome(self)
+        Battlefield.battle(self)
+
+battlefield = Battlefield()
+
