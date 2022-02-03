91933b5b46556e1e5bec6c7f52b4c63766b47bc9
h-latta <h.latta@yahoo.com>
2022-02-03 13:26:39 -0600

    Changed Weapon Mechanics when Assigned to Robot

--- robot.py
+++ robot.py
@@ -1,9 +1,9 @@
 from weapon import Weapon
 
-weapon = Weapon()
+sword = Weapon('Sword', 10)
 
 class Robot:
     def __init__(self, name):
         self.name = name
         self.health = 100
-        self.weapon = weapon('Sword', 10)
+        self.weapon = sword
