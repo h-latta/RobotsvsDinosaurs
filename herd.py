d5371aefd7c8b69865967714f1bee2e6a4207449
h-latta <h.latta@yahoo.com>
2022-02-03 13:31:57 -0600

    Added Roster to Herd

--- herd.py
+++ herd.py
@@ -1,10 +1,13 @@
 from dinosaur import Dinosaur
 
-dino = Dinosaur()
+dino_one = Dinosaur('Grog', 20)
+dino_two = Dinosaur('Rex', 15)
+dino_three = Dinosaur('Spike', 10)
 
 class Herd:
     def __init__(self):
-        self.herd = []
+        self.group = []
 
     def create_herd(self):
-        pass
+        dino_list = [dino_one.name, dino_two.name, dino_three.name]
+        self.group = dino_list
