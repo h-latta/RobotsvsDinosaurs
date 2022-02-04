27305abe1fafa798b1b73895b02b9d2314c317b9
h-latta <h.latta@yahoo.com>
2022-02-04 14:49:02 -0600

    Added Random Weapon Selector for Robots

--- robot.py
+++ robot.py
@@ -1,16 +1,23 @@
+import random
 from weapon import Weapon
 
-sword = Weapon('Sword', 25)
+sword = Weapon('Sword', 15)
+dino_blaster = Weapon('Dino Blaster', 25)
+club = Weapon('Club', 10)
+
+weapon_list = [sword, dino_blaster, club]
+
+random_weapon = random.choice(weapon_list)
 
 class Robot:
     def __init__(self, name):
         self.name = name
         self.health = 100
-        self.weapon = sword
+        self.weapon = random_weapon
 
     def attack(self, dinosaur):
         if dinosaur.health > 0:
             dinosaur.health = dinosaur.health - (self.weapon.attack_power)
-            print(f'{self.name} slashed {dinosaur.name} for {self.weapon.attack_power} damage!')
+            print(f'{self.name} used their {self.weapon.name} on {dinosaur.name} for {self.weapon.attack_power} damage!')
         else:
             pass
